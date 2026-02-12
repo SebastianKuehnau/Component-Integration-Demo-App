5cf8351c159cf766fa4c3e2b3a2633abf4e34ccb
Sebastian Kühnau <seb@vaadin.com>
2026-02-12 16:59:52 +0100

    Add rc-slider integration with custom styles and update theme configurations

--- src/main/java/org/vaadin/demo/Application.java
+++ src/main/java/org/vaadin/demo/Application.java
@@ -1,7 +1,11 @@
 package org.vaadin.demo;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.aura.Aura;
+import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,11 +16,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * and some desktop browsers.
  *
  */
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet("styles.css")
 @SpringBootApplication
-@Theme(value = "component-integration-demo-app")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
