2dcc5cc5216452674f49e91e692e010c139cbcf1
Sebastian Kühnau <seb@vaadin.com>
2026-02-16 16:32:35 +0100

    Add emoji click listener support to EmojiComponent and integrate with Tab4WebComponent

--- src/main/java/org/vaadin/demo/views/misc/components/EmojiComponent.java
+++ src/main/java/org/vaadin/demo/views/misc/components/EmojiComponent.java
@@ -1,17 +1,33 @@
 package org.vaadin.demo.views.misc.components;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.HasSize;
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.shared.Registration;
 
 @NpmPackage(value = "emoji-picker-element", version = "^1.26.3")
 @JsModule("emoji-picker-element/index.js")
 @Tag("emoji-picker")
 public class EmojiComponent extends Component implements HasSize {
 
-    public EmojiComponent() {
+    public Registration addEmojiClickListener(
+            ComponentEventListener<EmojiClickEvent> listener) {
+        return addListener(EmojiClickEvent.class, listener);
+    }
 
+    @DomEvent("emoji-click")
+    public static class EmojiClickEvent extends ComponentEvent<EmojiComponent> {
+        private final String unicode;
+
+        public EmojiClickEvent(EmojiComponent source,
+                               boolean fromClient,
+                               @EventData("event.detail.unicode") String unicode) {
+            super(source, fromClient);
+            this.unicode = unicode;
+        }
+
+        public String getUnicode() {
+            return unicode;
+        }
     }
 }
--- src/main/java/org/vaadin/demo/views/misc/tabs/Tab4WebComponent.java
+++ src/main/java/org/vaadin/demo/views/misc/tabs/Tab4WebComponent.java
@@ -1,5 +1,6 @@
 package org.vaadin.demo.views.misc.tabs;
 
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import org.vaadin.demo.views.misc.components.EmojiComponent;
 
@@ -7,7 +8,8 @@ public class Tab4WebComponent extends VerticalLayout {
 
     public Tab4WebComponent() {
         EmojiComponent emojiComponent = new EmojiComponent();
+        emojiComponent.addEmojiClickListener(event ->
+                Notification.show("Your current selection is: " + event.getUnicode()));
         add(emojiComponent);
-
     }
 }
